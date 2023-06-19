b91173a62bfc4007e4f8fe9179536b0b64d8c6da
Oleg Polikarpov <Vinches2193@yandex.ru>
2023-06-19 18:48:21 +0300

    debug

---
+++ test.py
@@ -0,0 +1,28 @@
+# users = {1: {"status_game": False,
+#                       "total_game": 0,
+#                       "wins_game": 0},
+#          2: {"status_game": False,
+#                       "total_game": 0,
+#                       "wins_game": 0},
+#          3: {"status_game": False,
+#                       "total_game": 0,
+#                       "wins_game": 0}
+#         }
+#
+# print(users[1])
+# print(len(users))
+
+some_list = [7, 14, 28, 32, 32, '56']
+
+def custom_filter(some_list):
+    res = 0
+    for i in some_list:
+        if i % 7 == 0 and i.isdigit():
+            res += i
+    if res > 83:
+        return False
+    else:
+        return True
+
+
+print(custom_filter(some_list))
