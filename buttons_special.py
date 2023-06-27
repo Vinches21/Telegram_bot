483b8d0e42848600041efca070fc7598c1ad08ea
Oleg Polikarpov <Vinches2193@yandex.ru>
2023-06-27 18:30:34 +0300

    Button-Web_app

--- buttons_special.py
+++ buttons_special.py
@@ -2,6 +2,7 @@ from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, Text
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, KeyboardButtonPollType, Message
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.types.web_app_info import WebAppInfo
 
 
 API_TOKEN: str = '5928240016:AAFu8zLiC8mstjLTzSlwd4i0vbeNRApFYNA'
@@ -25,13 +26,19 @@ poll_btn: KeyboardButton = KeyboardButton(
                                 text='Создать опрос/викторину',
                                 request_poll=KeyboardButtonPollType())
 
+web_app_btn: KeyboardButton = KeyboardButton(
+                                text='Start Web App',
+                                web_app=WebAppInfo(url="https://lookdecor.ru/"))
+
 # Добавляем кнопки в билдер
-kb_builder.row(contact_btn, geo_btn, poll_btn, width=1)
+kb_builder.row(contact_btn, geo_btn, poll_btn, web_app_btn, width=1)
 
 # Создаем объект клавиатуры
 keyboard: ReplyKeyboardMarkup = kb_builder.as_markup(
                                     resize_keyboard=True,
                                     one_time_keyboard=True)
+
+
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
@@ -43,6 +50,9 @@ async def process_start_command(message: Message):
 @dp.message(F.contact)
 async def process_answer_mobile(message: Message):
     await message.answer(text="Вот же дурак!!!")
+
+# Этот хэндлер будет срабатывать на команду "/web_app"
+
 
 if __name__ == '__main__':
     dp.run_polling(bot)
